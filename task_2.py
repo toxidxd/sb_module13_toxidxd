173830eadad6c9d9617419bea0f84232f11f9680
toxidxd <toxid13@yandex.ru>
2022-11-03 13:05:55 +0300

    done

--- task_2.py
+++ task_2.py
@@ -14,3 +14,17 @@ print('Задача 2. Функция максимума')
 # Напишите программу, 
 # которая находит максимум из трёх чисел.
 # Для этого используйте только функцию нахождения максимума из двух чисел.
+
+
+def max_number(a, b):
+    max_num = (a + b) / 2 + abs(a - b) / 2
+    return max_num
+
+
+firstNumber = int(input("Введите первое число: "))
+secondNumber = int(input("Введите второе число: "))
+thirdNumber = int(input("Введите третье число: "))
+
+
+maxNumber = max_number(max_number(firstNumber, secondNumber), thirdNumber)
+print(maxNumber)
