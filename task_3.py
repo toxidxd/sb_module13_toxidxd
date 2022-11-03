ab55b64b9744b811ffd50766eb32a0e10ea7ed28
toxidxd <toxid13@yandex.ru>
2022-11-03 13:38:13 +0300

    done

--- task_3.py
+++ task_3.py
@@ -4,7 +4,7 @@ print('Задача 3. Число наоборот 2')
 # Пользователь вводит два числа — N и K.
 # Напишите программу,
 # которая заменяет каждое число на число,
-# которое получается из исходного записью его цифр в обратном порядке,
+# полученное из исходного, записью его цифр в обратном порядке,
 # затем складывает их,
 # снова переворачивает и выводит ответ на экран.
 
@@ -19,3 +19,26 @@ print('Задача 3. Число наоборот 2')
  
 # Сумма: 522
 # Сумма наоборот: 225
+
+
+def flip_over(number):
+    result = 0
+
+    while number > 0:
+        digit = number % 10
+        result = result * 10 + digit
+        number //= 10
+    return result
+
+
+firstNumber = int(input("Введите первое число: "))
+secondNumber = int(input("Введите второе число: "))
+
+firstNumber_flip = flip_over(firstNumber)
+secondNumber_flip = flip_over(secondNumber)
+
+print(f"Первое число наоборот {firstNumber_flip}")
+print(f"Второе число наоборот {secondNumber_flip}\n")
+
+print(f"Сумма: {firstNumber + secondNumber}")
+print(f"Сумма наоборот: {firstNumber_flip + secondNumber_flip}")
